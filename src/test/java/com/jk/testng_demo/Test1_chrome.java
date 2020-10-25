f6480150632239a35468eadfad2ffc10f32310c4
unknown <jayakrishnan40u@gmail.com>
2020-10-25 21:19:46 +0530

    [added]

---
+++ src/test/java/com/jk/testng_demo/Test1_chrome.java
@@ -0,0 +1,40 @@
+package com.jk.testng_demo;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class Test1_chrome {
+
+	WebDriver driver;
+	
+	public  void initBrowser() throws MalformedURLException {
+		DesiredCapabilities cap = DesiredCapabilities.chrome();
+		cap.setPlatform(Platform.LINUX);		
+		URL url = new URL("http://192.168.99.104:4444/wd/hub");
+		driver = new RemoteWebDriver(url,cap);		
+	}
+	
+	@Test
+	public void test1() throws MalformedURLException {
+		initBrowser();
+		driver.get("http://google.com");
+		System.out.println("Title : "+ driver.getTitle());
+	}
+	
+	@Test
+	public void test2() {
+		driver.findElement(By.xpath("//input[@aria-label='Search' and @name='q']")).sendKeys("Hi Jk Here");
+		driver.close();	
+		driver.quit();
+	}
+	
+}
